87826f13e40bbf10680557d52449fadf37cd4e35
BeliefRC <beliefrc@outlook.com>
2019-12-03 17:48:29 +0800

    style：更改ListView中的方法排序方式

--- src/components/Listview/Listview.vue
+++ src/components/Listview/Listview.vue
@@ -55,13 +55,13 @@
   </scroll>
 </template>
 
-<script type="text/ecmascript-6">
-import Scroll from 'components/Scroll/Scroll'
-import Loading from 'components/Loading/Loading'
-import {getData} from 'common/js/dom'
+<script>
+import Scroll from "components/Scroll/Scroll";
+import Loading from "components/Loading/Loading";
+import { getData } from "common/js/dom";
 
-const TITLE_HEIGHT = 30
-const ANCHOR_HEIGHT = 18
+const TITLE_HEIGHT = 30;
+const ANCHOR_HEIGHT = 18;
 
 export default {
   components: {
@@ -71,20 +71,7 @@ export default {
   props: {
     data: {
       type: Array,
-      default: ()=>[]
-    }
-  },
-  computed: {
-    shortcutList() {
-      return this.data.map((group) => {
-        return group.title.substr(0, 1)
-      })
-    },
-    fixedTitle() {
-      if (this.scrollY > 0) {
-        return ''
-      }
-      return this.data[this.currentIndex] ? this.data[this.currentIndex].title : ''
+      default: () => []
     }
   },
   data() {
@@ -92,100 +79,116 @@ export default {
       scrollY: -1,
       currentIndex: 0,
       diff: -1
-    }
+    };
   },
   created() {
-    this.probeType = 3
-    this.listenScroll = true
-    this.touch = {}
-    this.listHeight = []
+    this.probeType = 3;
+    this.listenScroll = true;
+    this.touch = {};
+    this.listHeight = [];
   },
   methods: {
     selectItem(item) {
-      this.$emit('select', item)
+      this.$emit("select", item);
     },
     onShortcutTouchStart(e) {
-      let anchorIndex = getData(e.target, 'index')
-      let firstTouch = e.touches[0]
-      this.touch.y1 = firstTouch.pageY
-      this.touch.anchorIndex = anchorIndex
+      let anchorIndex = getData(e.target, "index");
+      let firstTouch = e.touches[0];
+      this.touch.y1 = firstTouch.pageY;
+      this.touch.anchorIndex = anchorIndex;
 
-      this._scrollTo(anchorIndex)
+      this._scrollTo(anchorIndex);
     },
     onShortcutTouchMove(e) {
-      let firstTouch = e.touches[0]
-      this.touch.y2 = firstTouch.pageY
-      let delta = (this.touch.y2 - this.touch.y1) / ANCHOR_HEIGHT | 0
-      let anchorIndex = parseInt(this.touch.anchorIndex) + delta
+      let firstTouch = e.touches[0];
+      this.touch.y2 = firstTouch.pageY;
+      let delta = ((this.touch.y2 - this.touch.y1) / ANCHOR_HEIGHT) | 0;
+      let anchorIndex = parseInt(this.touch.anchorIndex) + delta;
 
-      this._scrollTo(anchorIndex)
+      this._scrollTo(anchorIndex);
     },
     refresh() {
-      this.$refs.listView.refresh()
+      this.$refs.listView.refresh();
     },
     scroll(pos) {
-      this.scrollY = pos.y
+      this.scrollY = pos.y;
     },
     _calculateHeight() {
-      this.listHeight = []
-      const list = this.$refs.listGroup
-      let height = 0
-      this.listHeight.push(height)
+      this.listHeight = [];
+      const list = this.$refs.listGroup;
+      let height = 0;
+      this.listHeight.push(height);
       for (let i = 0; i < list.length; i++) {
-        let item = list[i]
-        height += item.clientHeight
-        this.listHeight.push(height)
+        let item = list[i];
+        height += item.clientHeight;
+        this.listHeight.push(height);
       }
     },
     _scrollTo(index) {
       if (!index && index !== 0) {
-        return
+        return;
       }
       if (index < 0) {
-        index = 0
+        index = 0;
       } else if (index > this.listHeight.length - 2) {
-        index = this.listHeight.length - 2
+        index = this.listHeight.length - 2;
       }
-      this.scrollY = -this.listHeight[index]
-      this.$refs.listView.scrollToElement(this.$refs.listGroup[index], 0)
+      this.scrollY = -this.listHeight[index];
+      this.$refs.listView.scrollToElement(this.$refs.listGroup[index], 0);
+    }
+  },
+  computed: {
+    shortcutList() {
+      return this.data.map(group => {
+        return group.title.substr(0, 1);
+      });
+    },
+    fixedTitle() {
+      if (this.scrollY > 0) {
+        return "";
+      }
+      return this.data[this.currentIndex]
+        ? this.data[this.currentIndex].title
+        : "";
     }
   },
   watch: {
     data() {
       setTimeout(() => {
-        this._calculateHeight()
-      }, 20)
+        this._calculateHeight();
+      }, 20);
     },
     scrollY(newY) {
-      const listHeight = this.listHeight
+      const listHeight = this.listHeight;
       // 当滚动到顶部，newY>0
       if (newY > 0) {
-        this.currentIndex = 0
-        return
+        this.currentIndex = 0;
+        return;
       }
       // 在中间部分滚动
       for (let i = 0; i < listHeight.length - 1; i++) {
-        let height1 = listHeight[i]
-        let height2 = listHeight[i + 1]
+        let height1 = listHeight[i];
+        let height2 = listHeight[i + 1];
         if (-newY >= height1 && -newY < height2) {
-          this.currentIndex = i
-          this.diff = height2 + newY
-          return
+          this.currentIndex = i;
+          this.diff = height2 + newY;
+          return;
         }
       }
       // 当滚动到底部，且-newY大于最后一个元素的上限
-      this.currentIndex = listHeight.length - 2
+      this.currentIndex = listHeight.length - 2;
     },
     diff(newVal) {
-      let fixedTop = (newVal > 0 && newVal < TITLE_HEIGHT) ? newVal - TITLE_HEIGHT : 0
+      let fixedTop =
+        newVal > 0 && newVal < TITLE_HEIGHT ? newVal - TITLE_HEIGHT : 0;
       if (this.fixedTop === fixedTop) {
-        return
+        return;
       }
-      this.fixedTop = fixedTop
-      this.$refs.fixed.style.transform = `translate3d(0,${fixedTop}px,0)`
+      this.fixedTop = fixedTop;
+      this.$refs.fixed.style.transform = `translate3d(0,${fixedTop}px,0)`;
     }
   }
-}
+};
 </script>
 
 <style scoped lang="stylus" rel="stylesheet/stylus">
