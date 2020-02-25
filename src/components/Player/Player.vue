505ef9f26c36ac678a6c95b277b90bc142e04004
BeliefRC <beliefrc@outlook.com>
2020-02-25 16:01:11 +0800

    feat(播放器): 完成播放器进度条的相关功能，展示与控制播放进度

--- src/components/Player/Player.vue
+++ src/components/Player/Player.vue
@@ -37,7 +37,12 @@
           </div>
           <div class="progress-wrapper">
             <span class="time time-l">{{ format(currentTime) }}</span>
-            <div class="progress-bar-wrapper"></div>
+            <div class="progress-bar-wrapper">
+              <ProgressBar
+                :percent="percent"
+                @percentChange="onProgressBarChange"
+              ></ProgressBar>
+            </div>
             <span class="time time-r">{{ format(currentSong.duration) }}</span>
           </div>
           <div class="operators">
@@ -70,7 +75,13 @@
           <p class="desc" v-html="currentSong.singer" />
         </div>
         <div class="control">
-          <i :class="miniPlayIcon" @click.stop="togglePlaying"></i>
+          <ProgressCircle :radius="radius" :percent="percent">
+            <i
+              :class="miniPlayIcon"
+              class="icon-mini"
+              @click.stop="togglePlaying"
+            ></i>
+          </ProgressCircle>
         </div>
         <div class="control" @click.stop="showPlaylist">
           <i class="icon-playlist" />
@@ -90,6 +101,8 @@
 <script>
 import { mapGetters, mapMutations } from "vuex";
 import animations from "create-keyframe-animation";
+import ProgressBar from "components/ProgressBar/ProgressBar";
+import ProgressCircle from "components/ProgressCircle/ProgressCircle";
 import * as types from "store/mutation-types";
 import { prefixStyle } from "common/js/dom";
 
@@ -98,7 +111,8 @@ export default {
   data() {
     return {
       songReady: false,
-      currentTime: 0
+      currentTime: 0,
+      radius: 32
     };
   },
   methods: {
@@ -156,6 +170,12 @@ export default {
       const minute = (interval / 60) | 0;
       const second = (interval % 60).toString().padStart(2, "0");
       return `${minute}:${second}`;
+    },
+    onProgressBarChange(percent) {
+      this.$refs.audio.currentTime = this.currentSong.duration * percent;
+      if (!this.playing) {
+        this.togglePlaying();
+      }
     },
     enter(el, done) {
       const { x, y, scale } = this._getPosAndScale();
@@ -233,6 +253,9 @@ export default {
     disableCls() {
       return this.songReady ? "" : "disable";
     },
+    percent() {
+      return this.currentTime / this.currentSong.duration;
+    },
     ...mapGetters([
       "fullScreen",
       "playlist",
@@ -253,6 +276,10 @@ export default {
         newPlaying ? audio.play() : audio.pause();
       });
     }
+  },
+  components: {
+    ProgressBar,
+    ProgressCircle
   }
 };
 </script>
@@ -270,6 +297,7 @@ export default {
         bottom: 0
         z-index: 150
         background: $color-background
+
         .background
             position: absolute
             left: 0
@@ -279,20 +307,24 @@ export default {
             z-index: -1
             opacity: 0.6
             filter: blur(20px)
+
         .top
             position: relative
             margin-bottom: 25px
+
             .back
                 position absolute
                 top: 0
                 left: 6px
                 z-index: 50
+
                 .icon-back
                     display: block
                     padding: 9px
                     font-size: $font-size-large-x
                     color: $color-theme
                     transform: rotate(-90deg)
+
             .title
                 width: 70%
                 margin: 0 auto
@@ -301,11 +333,13 @@ export default {
                 no-wrap()
                 font-size: $font-size-large
                 color: $color-text
+
             .subtitle
                 line-height: 20px
                 text-align: center
                 font-size: $font-size-medium
                 color: $color-text
+
         .middle
             position: fixed
             width: 100%
@@ -313,6 +347,7 @@ export default {
             bottom: 170px
             white-space: nowrap
             font-size: 0
+
             .middle-l
                 display: inline-block
                 vertical-align: top
@@ -320,22 +355,27 @@ export default {
                 width: 100%
                 height: 0
                 padding-top: 80%
+
                 .cd-wrapper
                     position: absolute
                     left: 10%
                     top: 0
                     width: 80%
                     height: 100%
+
                     .cd
                         width: 100%
                         height: 100%
                         box-sizing: border-box
                         border: 10px solid rgba(255, 255, 255, 0.1)
                         border-radius: 50%
+
                         &.play
                             animation: rotate 20s linear infinite
+
                         &.pause
                             animation-play-state: paused
+
                         .image
                             position: absolute
                             left: 0
@@ -349,35 +389,43 @@ export default {
                     margin: 30px auto 0 auto
                     overflow: hidden
                     text-align: center
+
                     .playing-lyric
                         height: 20px
                         line-height: 20px
                         font-size: $font-size-medium
                         color: $color-text-l
+
             .middle-r
                 display: inline-block
                 vertical-align: top
                 width: 100%
                 height: 100%
                 overflow: hidden
+
                 .lyric-wrapper
                     width: 80%
                     margin: 0 auto
                     overflow: hidden
                     text-align: center
+
                     .text
                         line-height: 32px
                         color: $color-text-l
                         font-size: $font-size-medium
+
                         &.current
                             color: $color-text
+
         .bottom
             position: absolute
             bottom: 50px
             width: 100%
+
             .dot-wrapper
                 text-align: center
                 font-size: 0
+
                 .dot
                     display: inline-block
                     vertical-align: middle
@@ -386,59 +434,80 @@ export default {
                     height: 8px
                     border-radius: 50%
                     background: $color-text-l
+
                     &.active
                         width: 20px
                         border-radius: 5px
                         background: $color-text-ll
+
             .progress-wrapper
                 display: flex
                 align-items: center
                 width: 80%
                 margin: 0 auto
                 padding: 10px 0
+
                 .time
                     color: $color-text
                     font-size: $font-size-small
                     flex: 0 0 30px
                     line-height: 30px
                     width: 30px
+
                     &.time-l
                         text-align: left
+
                     &.time-r
                         text-align: right
+
                 .progress-bar-wrapper
                     flex: 1
+
             .operators
                 display: flex
                 align-items: center
+
                 .icon
                     flex: 1
                     color: $color-theme
+
                     &.disable
                         color: $color-theme-d
+
                     i
                         font-size: 30px
+
                 .i-left
                     text-align: right
+
                 .i-center
                     padding: 0 20px
                     text-align: center
+
                     i
                         font-size: 40px
+
                 .i-right
                     text-align: left
+
                 .icon-favorite
                     color: $color-sub-theme
+
         &.normal-enter-active, &.normal-leave-active
             transition: all 0.4s
+
             .top, .bottom
                 transition: all 0.4s cubic-bezier(0.86, 0.18, 0.82, 1.32)
+
         &.normal-enter, &.normal-leave-to
             opacity: 0
+
             .top
                 transform: translate3d(0, -100px, 0)
+
             .bottom
                 transform: translate3d(0, 100px, 0)
+
     .mini-player
         display: flex
         align-items: center
@@ -449,20 +518,27 @@ export default {
         width: 100%
         height: 60px
         background: $color-highlight-background
+
         &.mini-enter-active, &.mini-leave-active
             transition: all 0.4s
+
         &.mini-enter, &.mini-leave-to
             opacity: 0
+
         .icon
             flex: 0 0 40px
             width: 40px
             padding: 0 10px 0 20px
+
             img
                 border-radius: 50%
+
                 &.play
                     animation: rotate 10s linear infinite
+
                 &.pause
                     animation-play-state: paused
+
         .text
             display: flex
             flex-direction: column
@@ -470,22 +546,27 @@ export default {
             flex: 1
             line-height: 20px
             overflow: hidden
+
             .name
                 margin-bottom: 2px
                 no-wrap()
                 font-size: $font-size-medium
                 color: $color-text
+
             .desc
                 no-wrap()
                 font-size: $font-size-small
                 color: $color-text-d
+
         .control
             flex: 0 0 30px
             width: 30px
             padding: 0 10px
+
             .icon-play-mini, .icon-pause-mini, .icon-playlist
                 font-size: 30px
                 color: $color-theme-d
+
             .icon-mini
                 font-size: 32px
                 position: absolute
